7943d531fcd34fca1b59ccf0abad0a9a3bf35ddb
Adam Coffman <coffman.adam@gmail.com>
2011-05-28 12:16:37 -0500

    updated ignorefile

---
+++ lib/scalableworkforce.rb
@@ -0,0 +1,13 @@
+%w{models.rb client.rb}.each do |file|
+  require File.dirname(__FILE__) +"/lib/" + file
+end
+
+module ScalableWorkforce
+  VERSION = '0.1.0'
+
+  BASE_URL = {
+    :sandbox => 'https://api.sandbox.scalableworkforce.com/xml',
+    :production => 'https://api.scalableworkforce.com/xml'
+  }
+
+end
